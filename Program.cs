e11eb8c6e6490fec9b69cd1ce98629ac53699e58
John Kougoulos <john.kougoulos@gmail.com>
2022-01-01 22:41:29 +0100

    first multi version

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@
 
 using Simple.OData.Client;
 using Microsoft.OData.Edm;
-using Microsoft.OData.Edm.Csdl;
-using Microsoft.OData.Edm.Csdl.CsdlSemantics;
+//using Microsoft.OData.Edm.Csdl;
+//using Microsoft.OData.Edm.Csdl.CsdlSemantics;
+using Microsoft.Data.Edm;
+
 
 using OData2Poco.Api;
 
@@ -11,42 +13,60 @@ using OData2Poco.Api;
 class Program
 {
 
-    private static string _svcurl = "https://services.odata.org/TripPinRESTierService/(S(jcidpq3drpzq0hefuabvtj5f))/";
+    private static string _svcurl = "https://services.odata.org/TripPinRESTierService/(S(jcidpq3drpzq0hefuabvtj5f))/"; // v4
+//    private static string _svcurl = "https://services.odata.org/V2/OData/OData.svc/"; // v2
     static async Task<int> Main(string[] args)
     {
 
         var cl = new ODataClient( _svcurl );
 
-        var model = (EdmModelBase) await cl.GetMetadataAsync();
-    
-        
-        var entitySets = model.EntityContainer.EntitySets();
 
-        foreach ( var entitySet in entitySets )
+        var modelabstr = await cl.GetMetadataAsync();
+
+        if ( modelabstr.GetType().FullName.StartsWith("Microsoft.Data.Edm") )
         {
-            Console.WriteLine( entitySet.Name + "/" + entitySet.EntityType().Name );
-
-            var type = entitySet.EntityType();
-            foreach ( var structProp in type.DeclaredStructuralProperties() )
+            Microsoft.Data.Edm.IEdmModel model =  modelabstr as Microsoft.Data.Edm.IEdmModel ;
+            
+            var entitySets = model.EntityContainers().FirstOrDefault().EntitySets();
+            foreach ( var entitySet in entitySets )
             {
-                if ( !structProp.Type.IsCollection() )
-                {
-                    var def = (IEdmNamedElement) structProp.Type.Definition;
-                    Console.WriteLine("---Prop: " + structProp.Name + " / " + def.Name );
-                }
-                else
-                {
-                    var def = (IEdmCollectionType) structProp.Type.Definition;
-                    var elemType = (IEdmNamedElement) def.ElementType.Definition; 
-                    Console.WriteLine("---Prop: " + structProp.Name + " / " + $"Collection({elemType.Name})" );
-                }
+                Console.WriteLine( entitySet.Name + "/" ); //+ entitySet );
             }
 
-            foreach ( var navProp in type.DeclaredNavigationProperties() )
-            {
-                Console.WriteLine("---NavProp: " + navProp.Name + " / " + navProp.Type.GetType().Name );
-            }
+        }
+        else
+        {
+            var model = (Microsoft.OData.Edm.EdmModelBase) await cl.GetMetadataAsync();
+        
+            var entitySets = model.EntityContainer.EntitySets();
 
+            foreach ( var entitySet in entitySets )
+            {
+                Console.WriteLine( entitySet.Name + "/" + entitySet.EntityType().Name );
+
+    
+                var type = entitySet.EntityType();
+                foreach ( var structProp in type.DeclaredStructuralProperties() )
+                {
+                    if ( !structProp.Type.IsCollection() )
+                    {
+                        var def = (Microsoft.OData.Edm.IEdmNamedElement) structProp.Type.Definition;
+                        Console.WriteLine("---Prop: " + structProp.Name + " / " + def.Name );
+                    }
+                    else
+                    {
+                        var def = (Microsoft.OData.Edm.IEdmCollectionType) structProp.Type.Definition;
+                        var elemType = (Microsoft.OData.Edm.IEdmNamedElement) def.ElementType.Definition; 
+                        Console.WriteLine("---Prop: " + structProp.Name + " / " + $"Collection({elemType.Name})" );
+                    }
+                }
+
+                foreach ( var navProp in type.DeclaredNavigationProperties() )
+                {
+                    Console.WriteLine("---NavProp: " + navProp.Name + " / " + navProp.Type.GetType().Name );
+                }
+            }
+    
 
 
         }
